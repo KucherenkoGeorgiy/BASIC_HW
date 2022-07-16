961b40ba85bb15eaf539bf7031e22fb2c813fe95
KucherenkoGeorgiy <georgiy.kucherenko@gmail.com>
2022-07-16 18:06:17 +0300

    added home work 8

---
+++ homework8/Task81.java
@@ -0,0 +1,34 @@
+package com.hillel.kucherenko.homework8;
+
+import java.util.Arrays;
+
+public class Task81 {
+    public static void main(String[] args) {
+        int[] arrayOfCompany = createArray(0, 9, 7);
+        int[] arrayOfPlayer = createArray(0, 9, 7);
+        int quantityOfMatches = 0;
+
+        Arrays.sort(arrayOfCompany);
+        Arrays.sort(arrayOfPlayer);
+
+        System.out.println(Arrays.toString(arrayOfCompany));
+        System.out.println(Arrays.toString(arrayOfPlayer));
+
+        for (int i = 0; i < arrayOfCompany.length; i++) {
+            if (arrayOfCompany[i] == arrayOfPlayer[i]) {
+                quantityOfMatches++;
+            }
+        }
+
+        System.out.println("Количество совпадений: " + quantityOfMatches);
+
+    }
+
+    static int[] createArray(int fromValue, int toValue, int quantityOfValues) {
+        int[] newArray = new int[quantityOfValues];
+        for (int i = 0; i < quantityOfValues; i++) {
+            newArray[i] = (int) (Math.random() * (toValue - fromValue + 1)) + fromValue;
+        }
+        return newArray;
+    }
+}
